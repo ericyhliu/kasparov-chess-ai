7f646cb96da5c9cd18d47a1868f943ec9deed682
Eric Liu <ericliu314@gmail.com>
2017-10-01 23:45:35 -0400

    Add move structure

--- src/com/kasparov/Main.java
+++ src/com/kasparov/Main.java
@@ -14,11 +14,27 @@ public class Main {
 
         boardStructure.updateListMaterials();
 
-        System.out.println("White attacking:");
-        SquareAttacked.showSqrAttackedBySide(BoardColor.WHITE.value, boardStructure);
+        System.out.println();
 
-        System.out.println("Black attacking:");
-        SquareAttacked.showSqrAttackedBySide(BoardColor.BLACK.value, boardStructure);
+        int move = 0;
+        int from = 6;
+        int to = 12;
+        int cap = BoardPiece.WHITE_ROOK.value;
+        int prom = BoardPiece.BLACK_ROOK.value;
+
+        move = (from | (to << 7) | (cap << 14) | (prom << 20));
+
+        System.out.println("Decimal: " + move);
+        System.out.println("    Hex: " + Integer.toHexString(move));
+        System.out.println(" Binary: " + Integer.toBinaryString(move));
+        System.out.println("   From: " + Move.from(move));
+        System.out.println("     To: " + Move.to(move));
+        System.out.println("Capture: " + Move.captured(move));
+        System.out.println("  Promo: " + Move.promoted(move));
+
+        //move |= Move.moveFlagPawnStart;
+
+        System.out.println("Is Pawn Start: " + ((move & Move.moveFlagPawnStart) != 0 ? "Yes" : "No"));
 
 
     }
---
+++ src/com/kasparov/Move.java
@@ -0,0 +1,38 @@
+package com.kasparov;
+
+/**
+ * Move structure.
+ *
+ * @author Eric Liu
+ */
+public class Move {
+    int move;
+    int score;
+
+    static int from(int move) {
+        return move & 0x3F;
+    }
+
+    static int to(int move) {
+        return (move >> 7) & 0x3F;
+    }
+
+    static int captured(int move) {
+        return (move >> 14) & 0xF;
+    }
+
+    static int promoted(int move) {
+        return (move >> 20) & 0xF;
+    }
+
+    static int moveFlagEnPassant = 0x40000;
+
+    static int moveFlagPawnStart = 0x80000;
+
+    static int moveFlagCastle = 0x1000000;
+
+    static int moveFlagCapture = 0x7C000;
+
+    static int moveFlagPromoted = 0xF00000;
+
+}
